d49aa21c0840169ba15d0e2278001ebb83fcce32
demirramazan <rdemir4434@gmail.com>
2017-01-27 10:25:29 +0300

    intellij text jsp function expression language

---
+++ src/function/ELFunction.java
@@ -0,0 +1,10 @@
+package function;
+
+/**
+ * Created by ramazan.demir on 26.1.2017.
+ */
+public class ELFunction {
+    public static int calculate(int n1, int n2) {
+        return (n1+n2)*5;
+    }
+}
